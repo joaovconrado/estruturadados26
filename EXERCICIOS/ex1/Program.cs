331e1f613dd4db71f7c4039c1f08d06e2663d3f2
unknown <jvconrado17@gmail.com>
2026-08-12 21:59:31 -0300

    12-08

--- EXERCICIOS/ex1/Program.cs
+++ EXERCICIOS/ex1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] imparnum;
+int[] parnumber;
+int i = 0;
+
+for(i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    if(numero % 2 = 0)
+    {
+        int[i] parnumber = numero;
+    }    
+}
+Console.WriteLine(parnumber);
